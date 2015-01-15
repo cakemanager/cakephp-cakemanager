834f1b4cd65c038370130aec54bacf040fb005e6
Bob Mulder <bobmulder96@gmail.com>
2015-01-15 15:40:40 +0100

    Added Behavior TestSuite

--- tests/TestCase/AllComponentsSuite.php
+++ tests/TestCase/AllComponentsSuite.php
@@ -12,9 +12,6 @@ class AllComponentsSuite extends TestSuite
         $suite = new self('Component related tests');
         $suite->addTestFile(__DIR__ . DS . 'Controller' . DS . 'Component' . DS . 'MenuComponentTest.php');
         $suite->addTestFile(__DIR__ . DS . 'Controller' . DS . 'Component' . DS . 'IsAuthorizedComponentTest.php');
-//        $suite->addTestDirectoryRecursive(__DIR__ . DS . 'Database');
-//        $suite->addTestDirectoryRecursive(__DIR__ . DS . 'ORM');
-//        $suite->addTestDirectoryRecursive(__DIR__ . DS . 'Model');
         return $suite;
     }
 
